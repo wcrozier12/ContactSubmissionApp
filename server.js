9994f453d39e3da99d954b714813d9ad1c5aeeda
wcrozier12 <wcc8@nau.edu>
2017-11-30 09:47:30 -0700

    added static route

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const app = express();
+const path = require('path');
 const PORT =  process.env.PORT || 3002;
 const routes = require('./routes/routes');
 const client = require('./config/connection');
-const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +12,11 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("public"));
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
